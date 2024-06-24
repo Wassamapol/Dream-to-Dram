2a82685b0fc4d5634c977ee3ca3c3ae4e5c96612
Wassamapol <bosswas1234@gmail.com>
2024-06-24 20:37:34 +0700

    first commit

---
+++ sprict.js
@@ -0,0 +1,11 @@
+var Answer1=5
+var Answer2=200
+var Answer3=80
+var Answer4=60 
+var Answer5=90 
+var Answer6=10 
+
+function showScore(score) {
+    const responseElement = document.getElementById("response");
+    responseElement.textContent = `You scored: ${score} points`;
+}
